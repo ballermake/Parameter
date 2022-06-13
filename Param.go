9e0bc5eb49f761c93a8d503e1f314995d963c60f
umvgc <ultramarinevolt@gmail.com>
2022-06-13 19:18:50 +0700

    update v2.0.0

--- Param.go
+++ Param.go
@@ -1,9 +1,17 @@
 package gage
 
-func CheckNum(num int) string {
-	if num%2 == 0 {
-		return "GENAP"
-	} else {
-		return "GANJIL"
+import "strings"
+
+func CheckNum(nums ...int) string {
+	var result = make([]string, len(nums))
+
+	for index, num := range nums {
+
+		if num%2 == 0 {
+			result[index] = "GENAP"
+		} else {
+			result[index] = "GANJIL"
+		}
 	}
+	return strings.Join(result, ", ")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/umvgc/Parameter
+module github.com/umvgc/Parameter/v2
 
 go 1.18
